70a767f32c8da9f193a7c8250246592d01baeb8e
matrix <261809340@qq.com>
2020-06-04 15:39:40 +0800

    增加了模型字段

--- blog/models.py
+++ blog/models.py
@@ -56,6 +56,55 @@ class Article(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='作者')
     views = models.PositiveIntegerField('阅读量', default=0)
     tui = models.ForeignKey(Tui, on_delete=models.DO_NOTHING, verbose_name='推荐位', blank=True, null=True)
+    created_time = models.DateTimeField('发布时间', auto_now_add=True)
+    modified_time = models.DateTimeField('修改时间', auto_now=True)
+
+    class Meta:
+        verbose_name = '文章'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.title
+
+
+# Banner背景图
+class Banner(models.Model):
+    text_info = models.CharField('标题', max_length=50, default='')
+    img = models.ImageField('轮播图', upload_to='banner/')
+    link_url = models.URLField('图片链接', max_length=200)
+    is_active = models.BooleanField('是否是active', default=False)
+
+    def __str__(self):
+        return self.text_info
+
+    class Meta:
+        verbose_name = '轮播图'
+        verbose_name_plural = '轮播图'
+
+
+# 友情链接
+class Link(models.Model):
+    name = models.CharField('链接名称', max_length=20)
+    link_url = models.URLField('网址', max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = '友情链接'
+        verbose_name_plural = '友情链接'
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
