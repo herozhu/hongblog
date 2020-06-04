7e06e49e3ee09024cc0834704f86ef4bfdd61294
matrix <261809340@qq.com>
2020-06-04 14:35:54 +0800

    修改了目录名

--- seting/asgi.py
+++ seting/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for hong project.
+ASGI config for seting project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hong.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seting.settings')
 
 application = get_asgi_application()
--- seting/wsgi.py
+++ seting/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hong project.
+WSGI config for seting project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hong.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seting.settings')
 
 application = get_wsgi_application()
