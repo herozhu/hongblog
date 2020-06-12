7e661181aa889d7d733249bf8b2b5c5dbde903ff
matrix <261809340@qq.com>
2020-06-12 18:46:11 +0800

    完成了列表页，内容页，标签页模板

--- blog/views.py
+++ blog/views.py
@@ -1,43 +1,72 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Category, Banner, Article, Tag
+from .models import Category, Banner, Article, Tag, Link
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def index(request):
     # 对Article进行声明并实例化，然后生成对象allarticle
-    allcategory = Category.objects.all()
-    allarticle = Article.objects.all().order_by('-id')[0:10]
-    banner = Banner.objects.filter(is_active=True)[0:4]  # 查询所有幻片灯图数据，并进行切片
+    allcategory = Category.objects.all()  # 查询出所有栏目分类
+    allarticle = Article.objects.all().order_by('-id')[0:10]  # 倒序查询出10条文章
+    banner = Banner.objects.filter(is_active=True)[0:4]  # 查询所有幻片灯图数据，并进行切片，显示4章图片
     tui = Article.objects.filter(tui_id=1)[:3]  # 查询推荐位ID为1的文章
     hot = Article.objects.all().order_by('views')[:10]  # 通过浏览数进行排序
-    remen = Article.objects.filter(tui_id=2)[:6]
-    tags = Tag.objects.all()
-    # 把查询到的对象，封装到上下文
-    context = {
-        'allcategory': allcategory,
-        'allarticle': allarticle,
-        'hot': hot,
-        'remen': remen,
-        'banner': banner,
-        'tui': tui,
-        'tags': tags
-        }
+    remen = Article.objects.filter(tui_id=2)[:6]  # 通过后台推荐位ID=2的热门推荐，显示6条相关的文章
+    tags = Tag.objects.all()  # 查询所有的标签
+    link = Link.objects.all()  # 查询所有的友情链接
 
-    # 把上下文传递到模板里
-    return render(request, 'default/index.html', context)
+    #  locals()的作用是返回一个包含当前作用域里面的所有变量和它们的值的字典,优化了context = {...}的方式。
+    return render(request, 'default/index.html', locals())
 
 
 def list(request, lid):
+    list = Article.objects.filter(category_id=lid)  # 获取通过URL传进来的lid，然后筛选出对应文章
+    cname = Category.objects.get(id=lid)  # 获取当前文章的栏目名
+    remen = Article.objects.filter(tui_id=2)[:6]  # 通过后台推荐位ID=2的热门推荐，显示6条相关的文章
+    allcategory = Category.objects.all()  # 查询出所有栏目分类
+    tags = Tag.objects.all()  # 查询所有的标签
+    #  分页器的实现
+    page = request.GET.get('page')  # 在URL中获取当前页面数
+    paginator = Paginator(list, 5)  # 对查询到的数据对象list进行分页，设置超过5条数据就分页
+    try:
+            list = paginator.page(page)
+    except PageNotAnInteger:
+           list = paginator.page(1)  # 如果用户输入的页码不是整数时,显示第1页的内容
+    except EmptyPage:
+           list = paginator.page(paginator.num_pages)  # 如果用户输入的页数不在系统的页码列表中时,显示最后一页的内容
 
-    pass
+    #  locals()的作用是返回一个包含当前作用域里面的所有变量和它们的值的字典,优化了context = {...}的方式。
+    return render(request, 'default/list.html', locals())
 
 
 def show(request, sid):
-    pass
+    show = Article.objects.get(id=sid)  # 查询指定ID的文章
+    allcategory = Category.objects.all()  # 获取导航上的所有分类
+    tags = Tag.objects.all()  # 获取右侧上的所有标签
+    remen = Article.objects.filter(tui_id=2)[:6]  # 获取6条推荐位为热门的文章
+    hot = Article.objects.all().order_by('?')[:10]  # 内容下面的您可能感兴趣的文章，随机推荐
+    previous_blog = Article.objects.filter(created_time__gt=show.created_time, category=show.category.id).first()
+    next_blog = Article.objects.filter(created_time__lt=show.created_time, category=show.category.id).last()
+    show.views = show.views + 1
+    show.save()
+    return render(request, 'default/show.html', locals())
 
 
 def tag(request, tag):
-    pass
+    list = Article.objects.filter(tags__name=tag)  # 通过文章标签进行查询文章
+    remen = Article.objects.filter(tui_id=2)[:6]  # 通过后台推荐位ID=2的热门推荐，显示6条相关的文章
+    allcategory = Category.objects.all()
+    tname = Tag.objects.get(name=tag)  # 获取当前搜索的标签名
+    page = request.GET.get('page')
+    tags = Tag.objects.all()
+    paginator = Paginator(list, 5)  # 对查询到的数据对象list进行分页，设置超过5条数据就分页
+    try:
+        list = paginator.page(page)  # 获取当前页码的记录
+    except PageNotAnInteger:
+        list = paginator.page(1)  # 如果用户输入的页码不是整数时,显示第1页的内容
+    except EmptyPage:
+        list = paginator.page(paginator.num_pages)  # 如果用户输入的页数不在系统的页码列表中时,显示最后一页的内容
+    return render(request, 'default/tags.html', locals())
 
 
 def search(request, search):
--- hong/urls.py
+++ hong/urls.py
@@ -25,10 +25,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),  # 管理后台
     path('', views.index, name='index'),  # 网站首页
     path('list-<int:lid>.html', views.list, name='list'),  # 列表页
-    path('show-<int:lid>.html', views.show, name='show'),  # 内容页
+    path('show-<int:sid>.html', views.show, name='show'),  # 内容页
     path('tag/<tag>', views.tag, name='tags'),  # 标签列表页
     path('s/', views.search, name='search'),  # 搜索列表页
     path('about/', views.about, name='about'),  # 联系我们单页
     path('ueditor/', include('DjangoUeditor.urls')),  # 添加DjangoUeditor的URL
-    re_path('^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),  # 增加此行
+    re_path('^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),  # 设置上传图片文档路径
 ]
