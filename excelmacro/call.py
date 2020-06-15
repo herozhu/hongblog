8b2c8f61096efb55b0b430533407fadeb52c455d
matrix <261809340@qq.com>
2020-06-15 16:21:27 +0800

    完成了搜索页，单页的制作

--- excelmacro/call.py
+++ excelmacro/call.py
@@ -1,19 +1,15 @@
 import xlwings as xw
+from blog import views
 #  调用表格宏命令
 
 
-wb = xw.Book(r'E:\\hong-v1.0\\主算表.xlsm')
+wb = xw.Book(r'../outcome/001.xlsx')
 sheet = wb.sheets('Sheet1')
-sheet.range('F24').value = '细粒土'
-macro_var_1 = '模板确认'
-macro_var_2 = '数据生成'
-macro_var_3 = '数据成果导出'
-macro_1 = wb.macro(macro_var_1)
-macro_2 = wb.macro(macro_var_2)
-macro_3 = wb.macro(macro_var_3)
-macro_1()
-macro_2()
-macro_3()
+
+v1 = views.search().get()
+sheet.range('A1').value = v1
+
+
 
 
 
